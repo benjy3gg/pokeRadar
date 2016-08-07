613b5f33ee52042f04a098e8eb176e8999c5e347
benjy3gg <benjy3gg@googlemail.com>
2016-08-07 20:43:57 +0200

    clean

--- app/src/main/java/com/benjy3gg/livemapgo/ChangeBubbleEvent.java
+++
@@ -1,9 +0,0 @@
-package com.benjy3gg.livemapgo;
-
-public class ChangeBubbleEvent {
-    public final String whattodo;
-
-    public ChangeBubbleEvent(String whattodo) {
-        this.whattodo = whattodo;
-    }
-}
